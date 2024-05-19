b656fd0111a597f81a87559e6b654d09833f8262
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-05-19 19:40:40 +0530

    Updated Resume section

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,21 +2,23 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Jagadeesh_B_ECE_.pdf";
+import pdf from "../../Assets/Jagadeesh_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://drive.google.com/file/d/1iCgg8nwS0XwQsXJBDNyPK1V-mypMbzu4/view?usp=sharing";
-
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState(null);
 
   useEffect(() => {
     setWidth(window.innerWidth);
   }, []);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
 
   return (
     <div>
@@ -35,8 +37,18 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document
+            file={pdf}
+            onLoadSuccess={onDocumentLoadSuccess}
+            className="d-flex justify-content-center"
+          >
+            {[...Array(numPages).keys()].map((page) => (
+              <Page
+                key={page}
+                pageNumber={page + 1}
+                scale={width > 786 ? 1.7 : 0.6}
+              />
+            ))}
           </Document>
         </Row>
 
