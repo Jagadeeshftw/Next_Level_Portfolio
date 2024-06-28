481c5357474e17b7ab8ae4992668d99a0f319ef6
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-29 02:12:07 +0530

    Importing polyfill

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import MoonImg from "../public/moon2.png";
 import Footer from "@/components/Footer";
 import "./globals.css";
+import "../utils/polyfills";
 
 const inter = Inter({ subsets: ["latin"] });
 
