52591e9318bb33779ddfcdead2981fc29c42a338
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-06-29 11:26:42 +0530

    Adjusting github calender responsiveness

--- components/about/Github.tsx
+++ components/about/Github.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import GitHubCalendar, { ThemeInput } from "react-github-calendar";
 
 function Github() {
-  // @ts-nocheck
+  // Define your themeInput
   const themeInput: ThemeInput = {
     light: ["#f0f0f0", "#c4edde", "#7ac7c4", "#f73859", "#384259"],
     dark: ["#FFFFFF", "#6B7280", "#1F2937", "#111827", "#030712"],
   };
 
   return (
-    <div className="flex justify-center pb-10">
-      <div>
+    <div className="flex justify-center pb-10 px-2">
+      <div className="max-w-screen-xl w-full">
         <GitHubCalendar
           username="jagadeeshftw"
           blockSize={16}
@@ -18,6 +18,7 @@ function Github() {
           colorScheme={"dark"}
           theme={themeInput}
           fontSize={16}
+          style={{ objectFit: "cover", width: "100%" }}
         />
       </div>
     </div>
