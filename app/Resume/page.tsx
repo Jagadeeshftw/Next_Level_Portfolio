3d8e68db3b3ea10bc0fa273da65dc727145101a5
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-07-31 02:27:56 +0530

    Move footer from layout to page

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -5,6 +5,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { Button } from "@/components/ui/moving-border";
+import Footer from "@/components/Footer";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
@@ -16,27 +17,31 @@ function ResumeNew() {
   }, []);
 
   return (
-    <div className="container mx-auto py-12 resume-section">
-      <div className="flex justify-center my-4">
-        <Button
-          borderRadius="1.75rem"
-          as="a"
-          href="/JagadeeshB.pdf"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-white  dark:bg-zinc-600 text-black px-5 py-3 dark:text-white border-neutral-200 dark:border-slate-800"
-        >
-          <AiOutlineDownload className="inline mr-2" />
-          Download CV
-        </Button>
+    <>
+      <div className="container mx-auto py-12 resume-section">
+        <div className="flex justify-center my-4">
+          <Button
+            borderRadius="1.75rem"
+            as="a"
+            href="/JagadeeshB.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-white  dark:bg-zinc-600 text-black px-5 py-3 dark:text-white border-neutral-200 dark:border-slate-800"
+          >
+            <AiOutlineDownload className="inline mr-2" />
+            Download CV
+          </Button>
+        </div>
+
+        <div className="resume">
+          <Document file="/JagadeeshB.pdf" className="flex justify-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </div>
       </div>
 
-      <div className="resume">
-        <Document file="/JagadeeshB.pdf" className="flex justify-center">
-          <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-        </Document>
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
