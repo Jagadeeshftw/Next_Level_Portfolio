3b62bc472fe51a38d8f01f0bcfe5a311f7f6b0c1
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-07-31 02:32:06 +0530

    Update Programming Languages

--- components/about/ProgrammingLanguages.tsx
+++ components/about/ProgrammingLanguages.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
 import { CgCPlusPlus } from "react-icons/cg";
-import { DiJavascript1, DiPython, DiCss3 } from "react-icons/di";
-import { SiSolidity, SiTypescript, SiC, SiHtml5 } from "react-icons/si";
+import { DiJavascript1, DiPython } from "react-icons/di";
+import { SiSolidity, SiTypescript } from "react-icons/si";
 import { GlareCard } from "../ui/glare-card";
-
+import { FaRust } from "react-icons/fa";
 function ProgrammingLanguages() {
   return (
     <div className="flex flex-wrap justify-center gap-10">
@@ -13,32 +13,24 @@ function ProgrammingLanguages() {
         <p className="text-white font-bold text-xl mt-4">Solidity</p>
       </GlareCard>
       <GlareCard className="flex flex-col items-center justify-center">
-        <DiJavascript1 className="w-2/3 h-2/3 p-8" />
-        <p className="text-white font-bold text-xl mt-4">Javascript</p>
+        <FaRust className="w-2/3 h-2/3 p-8" />
+        <p className="text-white font-bold text-xl mt-4">Rust</p>
       </GlareCard>
       <GlareCard className="flex flex-col items-center justify-center">
         <SiTypescript className="w-2/3 h-2/3 p-8" />
         <p className="text-white font-bold text-xl mt-4">Typescript</p>
       </GlareCard>
       <GlareCard className="flex flex-col items-center justify-center">
+        <DiJavascript1 className="w-2/3 h-2/3 p-8" />
+        <p className="text-white font-bold text-xl mt-4">Javascript</p>
+      </GlareCard>
+      <GlareCard className="flex flex-col items-center justify-center">
         <CgCPlusPlus className="w-2/3 h-2/3 p-8" />
         <p className="text-white font-bold text-xl mt-4">C++</p>
       </GlareCard>
       <GlareCard className="flex flex-col items-center justify-center">
-        <SiC className="w-2/3 h-2/3 p-8" />
-        <p className="text-white font-bold text-xl mt-4">C</p>
-      </GlareCard>
-      <GlareCard className="flex flex-col items-center justify-center">
         <DiPython className="w-2/3 h-2/3 p-8" />
         <p className="text-white font-bold text-xl mt-4">Python</p>
-      </GlareCard>
-      <GlareCard className="flex flex-col items-center justify-center">
-        <SiHtml5 className="w-2/3 h-2/3 p-8" />
-        <p className="text-white font-bold text-xl mt-4">Html</p>
-      </GlareCard>
-      <GlareCard className="flex flex-col items-center justify-center">
-        <DiCss3 className="w-2/3 h-2/3 p-8" />
-        <p className="text-white font-bold text-xl mt-4">Css</p>
       </GlareCard>
     </div>
   );
