28ea2655b6f57a5e30042ecfd36908f43df820de
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-07-31 02:31:35 +0530

    Update databases

--- components/about/Databases.tsx
+++ components/about/Databases.tsx
@@ -2,9 +2,19 @@ import React from "react";
 
 import { SiMysql, SiMongodb, SiSequelize } from "react-icons/si";
 import { GlareCard } from "../ui/glare-card";
+import { SiIpfs } from "react-icons/si";
+import { SiPrisma } from "react-icons/si";
 function Databases() {
   return (
     <div className="flex flex-wrap justify-center gap-10">
+      <GlareCard className="flex flex-col items-center justify-center">
+        <SiIpfs className="w-2/3 h-2/3 p-8" />
+        <p className="text-white font-bold text-xl mt-4">IPFS</p>
+      </GlareCard>
+      <GlareCard className="flex flex-col items-center justify-center">
+        <SiPrisma className="w-2/3 h-2/3 p-8" />
+        <p className="text-white font-bold text-xl mt-4">Prisma</p>
+      </GlareCard>
       <GlareCard className="flex flex-col items-center justify-center">
         <SiMysql className="w-2/3 h-2/3 p-8" />
         <p className="text-white font-bold text-xl mt-4">MySQL</p>
