f7d507854cd5bd5ef6d3a7b98a374d4717eb6f18
Jagadeeshftw <jagadeesh26062002@gmail.com>
2024-07-31 02:31:48 +0530

    Update frameworks

--- components/about/Frameworks.tsx
+++ components/about/Frameworks.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { DiReact, DiNodejs } from "react-icons/di";
-import { SiWeb3Dotjs, SiBootstrap, SiExpress, SiRedux } from "react-icons/si";
-import { FaEthereum } from "react-icons/fa";
+import { SiWeb3Dotjs, SiExpress } from "react-icons/si";
+import { FaEthereum, FaHardHat } from "react-icons/fa";
 import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
 import { GlareCard } from "../ui/glare-card";
-
+import { SiPolygon, SiEthers } from "react-icons/si";
 function Frameworks() {
   return (
     <div className="flex flex-wrap justify-center gap-10">
@@ -13,16 +13,20 @@ function Frameworks() {
         <p className="text-white font-bold text-xl mt-4">Ethereum</p>
       </GlareCard>
       <GlareCard className="flex flex-col items-center justify-center">
+        <SiPolygon className="w-2/3 h-2/3 p-8" />
+        <p className="text-white font-bold text-xl mt-4">Polygon</p>
+      </GlareCard>
+      <GlareCard className="flex flex-col items-center justify-center">
+        <FaHardHat className="w-2/3 h-2/3 p-8" />
+        <p className="text-white font-bold text-xl mt-4">Hardhat</p>
+      </GlareCard>
+      <GlareCard className="flex flex-col items-center justify-center">
+        <SiEthers className="w-2/3 h-2/3 p-8" />
+        <p className="text-white font-bold text-xl mt-4">Ethers.js</p>
+      </GlareCard>
+      <GlareCard className="flex flex-col items-center justify-center">
         <SiWeb3Dotjs className="w-2/3 h-2/3 p-8" />
         <p className="text-white font-bold text-xl mt-4">Web3.js</p>
-      </GlareCard>
-      <GlareCard className="flex flex-col items-center justify-center">
-        <DiReact className="w-2/3 h-2/3 p-8" />
-        <p className="text-white font-bold text-xl mt-4">React</p>
-      </GlareCard>
-      <GlareCard className="flex flex-col items-center justify-center">
-        <SiRedux className="w-2/3 h-2/3 p-8" />
-        <p className="text-white font-bold text-xl mt-4">Redux</p>
       </GlareCard>
       <GlareCard className="flex flex-col items-center justify-center">
         <RiNextjsFill className="w-2/3 h-2/3 p-8" />
@@ -39,10 +43,6 @@ function Frameworks() {
       <GlareCard className="flex flex-col items-center justify-center">
         <RiTailwindCssFill className="w-2/3 h-2/3 p-8" />
         <p className="text-white font-bold text-xl mt-4">Tailwind</p>
-      </GlareCard>
-      <GlareCard className="flex flex-col items-center justify-center">
-        <SiBootstrap className="w-2/3 h-2/3 p-8" />
-        <p className="text-white font-bold text-xl mt-4">Bootstrap</p>
       </GlareCard>
     </div>
   );
